301fbdddf2f8006c7cef7aa8e1e93029edab997c
GityHuby <meetsuriya@gmail.com>
2014-09-24 20:18:04 +0530

    Point type showing increase in Emission and others show decrease

---
+++ project2/plot3.R
@@ -0,0 +1,20 @@
+#Script to find the PM2.5 emmission trend for various type of sources
+
+#Install plyr package before running the code. >install.packages("plyr")
+library(plyr);
+
+NEI <- readRDS("exdata_data_NEI_data/summarySCC_PM25.rds");
+SCC <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds");
+
+frameBCML <-NEI[NEI$fips=="24510",];
+res <- ddply(frameBCML, c("year","type") , summarize, sum = sum(Emissions));
+
+g <- ggplot(res, aes(year,sum));
+g <- g + theme(plot.background = element_rect(fill = 'lightsteelblue1'));
+g <- g + theme(panel.background = element_rect(fill = 'lightsteelblue3')); 
+g <- g + geom_point(colour="midnightblue", shape="*", size=10) + geom_line(linetype="solid", colour="midnightblue", size=2); #alpha, 
+g <- g + ggtitle("Emission Trend for various Type of Sources")+xlab("Year")+ylab("Total Emission");
+g <- g+ facet_grid(.~type);
+png("plot3.png", width=1000, height=600);
+print(g);
+dev.off();
